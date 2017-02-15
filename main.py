2bf653155140f30259d72bf42bbb432f772add12
Nate Fonseka <natefonseka@gmail.com>
2017-02-15 17:56:07 -0500

    adding more logic that will hopefully make app more k8s friendly.

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from boto.route53.record import ResourceRecordSets
 ZONE_ID = os.environ['CROW_ZONE_ID']  # k8s secret later
 DNS = os.environ['CROW_DNS']  # need to get from secret later
 NODE_IP = os.environ['CROW_NODE_IP']  # need to get from k8s secret later
+AWS_REGION = os.getenv('AWS_REGION', 'us-west-2')
 app = Flask(__name__)
-conn = boto.route53.connect_to_region('us-west-2')
+conn = boto.route53.connect_to_region(AWS_REGION)
 change_set = ResourceRecordSets(conn, ZONE_ID)
 
 
@@ -17,12 +18,11 @@ change_set = ResourceRecordSets(conn, ZONE_ID)
 def main():
     data = request.json
     branch = data["object_attributes"]["source_branch"]
-
     action = data["object_attributes"]["state"]
-    if (action == 'opened' or action == 'reopened'):
+    if action == 'opened' or action == 'reopened':
         log.info('PR opened, creating DNS records + k8s deploy for branch' + branch)
         opened(branch)
-    elif (action == 'closed'):
+    elif action == 'closed':
         log.info('PR closed, deleting DNS records + k8s deploy for branch' + branch)
         closed(branch)
     return 'OK'
