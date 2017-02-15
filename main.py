c84468c411b06f7a0f8225396040cedad23f4025
Nate Fonseka <natefonseka@gmail.com>
2017-02-15 16:17:37 -0500

    adding configurable main.py. realigned the data fetching to geared towards gitlab. will need to push it towards github latere

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import request
+import logging as log
 import boto.route53
+import os
 from boto.route53.record import ResourceRecordSets
 
-ZONE_ID = 'NEED TO CHANGE LATER'  # k8s secret later
-DNS = 'example.com'  # need to get from secret later
-NODE_IP = '123.1.1'  # need to get from k8s secret later
+ZONE_ID = os.environ['CROW_ZONE_ID']  # k8s secret later
+DNS = os.environ['CROW_DNS']  # need to get from secret later
+NODE_IP = os.environ['CROW_NODE_IP']  # need to get from k8s secret later
 app = Flask(__name__)
 conn = boto.route53.connect_to_region('us-west-2')
 change_set = ResourceRecordSets(conn, ZONE_ID)
@@ -14,13 +16,17 @@ change_set = ResourceRecordSets(conn, ZONE_ID)
 @app.route('/', methods=['POST'])
 def main():
     data = request.json
-    branch = data.branch
+    branch = data["object_attributes"]["source_branch"]
 
-    action = data.action
+    action = data["object_attributes"]["state"]
     if (action == 'opened' or action == 'reopened'):
+        log.info('PR opened, creating DNS records + k8s deploy for branch' + branch)
         opened(branch)
     elif (action == 'closed'):
-        closed(branch);
+        log.info('PR closed, deleting DNS records + k8s deploy for branch' + branch)
+        closed(branch)
+    return 'OK'
+
 
 def opened(branch):
     '''
@@ -28,16 +34,17 @@ def opened(branch):
      then we will need to create a svc for it + ingress rules
      finally create a r53 record
     '''
-    changes1 = change_set.add_change("UPSERT", branch + DNS, type="CNAME", ttl=1000)
+    changes1 = change_set.add_change("UPSERT", branch + '.' + DNS, type="A", ttl=300)
     changes1.add_value(NODE_IP)
     change_set.commit()
+
 
 def closed(branch):
     '''
     We will need to get ingress, and then remove the ingress rule for this dns.
     delete deployments from this branch, as well as remove r53 record
     '''
-    changes1 = change_set.add_change("DELETE", branch + DNS, type="CNAME", ttl=1000)
+    changes1 = change_set.add_change("DELETE", branch + '.' + DNS, type="A", ttl=300)
     changes1.add_value(NODE_IP)
     change_set.commit()
 
