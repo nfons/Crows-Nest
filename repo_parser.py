eee4edc0a322bf3fa41fba63446b116db9677c98
Nate Fonseka <natefonseka@gmail.com>
2017-03-08 08:59:58 -0500

    adding logic to get comment url from payload. will be used to comment on the PR

--- repo_parser.py
+++ repo_parser.py
@@ -6,7 +6,15 @@ def gitlab(payload):
     object["image"] = image
     object["action"] = action
     object["branch"] = branch
+    # stupid gitab comment_url you have to build by hand
+    base_url = payload["object_attributes"]["source"]["web_url"]
+    base_url = base_url.replace(image, '')
+    merge_request_id = payload["object_attributes"]["id"]
+    project_id = payload["object_attributes"]["target_project_id"]
+    url = base_url + '/projects/' + project_id + '/merge_requests/' + merge_request_id + '/notes'
+    object['comment_url'] = url
     return object
+
 
 def github(payload):
     object = {}
@@ -16,10 +24,12 @@ def github(payload):
     object["image"] = image
     object["action"] = action
     object["branch"] = branch
+    object["comment_url"] = payload['comments_url']
     return object
+
 
 def getRepo(type, payload):
     if type == 'github':
-       return github(payload)
+        return github(payload)
     else:
         return gitlab(payload)
